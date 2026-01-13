77c17b80c5069a97cc303c0717b78fc01164b46d
dhany <dhany@citra-digital.org>
2026-01-13 11:03:05 +0700

    feat: expect with chai

--- test/CryptoZombies.js
+++ test/CryptoZombies.js
@@ -1,6 +1,7 @@
 const ZombieOwnership = artifacts.require("ZombieOwnership");
 const utils = require("./helpers/utils");
 const time = require("./helpers/time");
+const expect = require("chai").expect;
 
 const zombieNames = ["Zombie 1", "Zombie 2"];
 
@@ -16,8 +17,8 @@ contract("ZombieOwnership", (accounts) => {
         const result = await contractInstance.createRandomZombie(zombieNames[0], {
             from: alice
         })
-        assert.equal(result.receipt.status, true);
-        assert.equal(result.logs[0].args.name, zombieNames[0]);
+        expect(result.receipt.status).to.equal(true);
+        expect(result.logs[0].args.name).to.equal(zombieNames[0]);
     })
 
 
@@ -37,7 +38,7 @@ contract("ZombieOwnership", (accounts) => {
             const zombieId = result.logs[0].args.zombieId.toNumber();
             await contractInstance.transferFrom(alice, bob, zombieId, {from: alice});
             const newOwner = await contractInstance.ownerOf(zombieId);
-            assert.equal(newOwner, bob);
+            expect(newOwner).to.equal(bob);
         })
     })
 
@@ -48,7 +49,7 @@ contract("ZombieOwnership", (accounts) => {
             await contractInstance.approve(bob, zombieId, {from:alice});
             await contractInstance.transferFrom(alice, bob, zombieId, {from: bob});
             const newOwner = await contractInstance.ownerOf(zombieId);
-            assert.equal(newOwner,bob);
+            expect(newOwner).to.equal(bob);
         })
         
         it("should approve and then transfer a zombie when the owner calls transferFrom", async () => {
@@ -57,7 +58,7 @@ contract("ZombieOwnership", (accounts) => {
             await contractInstance.approve(bob, zombieId, {from: alice});
             await contractInstance.transferFrom(alice, bob, zombieId, {from: alice});
             const newOwner = await contractInstance.ownerOf(zombieId);
-            assert.equal(newOwner,bob);
+            expect(newOwner).to.equal(bob);
         })
     })
 
@@ -69,7 +70,7 @@ contract("ZombieOwnership", (accounts) => {
         const secondZombieId = result.logs[0].args.zombieId.toNumber();
         await time.increase(time.duration.days(1));
         await contractInstance.attack(firstZombieId, secondZombieId, {from: alice});
-        assert.equal(result.receipt.status, true);
+        expect(result.receipt.status).to.equal(true);
     })
 
 })
